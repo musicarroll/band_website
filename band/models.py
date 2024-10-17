e07855b6851036ea54125365ac37a3c91c5978b6
Michael Carroll <musicarroll@gmail.com>
2024-10-17 17:22:39 -0500

    Musicians

--- band/models.py
+++ band/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
+
+from PIL import Image
+import os
 
 class Musician(models.Model):
     name = models.CharField(max_length=100)
@@ -9,8 +11,32 @@ class Musician(models.Model):
     bio = models.TextField()
     photo = models.ImageField(upload_to='musicians/', blank=True, null=True)
 
+    def save(self, *args, **kwargs):
+        # Call the original save method to ensure the photo is saved first
+        super().save(*args, **kwargs)
+
+        # Resize the photo if it exists
+        if self.photo:
+            img_path = self.photo.path
+            max_size = (300, 300)  # Set the desired max dimensions
+
+            # Open the image using Pillow
+            with Image.open(img_path) as img:
+                if img.height > max_size[1] or img.width > max_size[0]:
+                    img.thumbnail(max_size, Image.Resampling.LANCZOS)
+                    img.save(img_path)  # Save resized image back to disk
+
     def __str__(self):
         return self.name
+
+# class Musician(models.Model):
+#     name = models.CharField(max_length=100)
+#     instrument = models.CharField(max_length=100)
+#     bio = models.TextField()
+#     photo = models.ImageField(upload_to='musicians/', blank=True, null=True)
+
+#     def __str__(self):
+#         return self.name
 
 class AudioClip(models.Model):
     title = models.CharField(max_length=100)
